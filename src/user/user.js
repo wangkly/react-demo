e4285ed1d94137fbf2d3e54ec7113600417dd33d
wangkly <wanggang@qianmi.com>
2019-04-24 18:09:23 +0800

    modify

--- src/user/user.js
+++ src/user/user.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import {Button} from 'antd';
 
 export default class User extends Component{
 
@@ -14,7 +15,16 @@ export default class User extends Component{
                     <div className="user-headimg">
                         <img src ="https://pic.qianmi.com/qmui/v0.2/img/avatar-default.png" />   
                     </div>
+
+                    <div className="user-info">
+                        <h2>wangkly</h2>
+                        <h4>wangkly,wangkly</h4>
+                    </div>
                     
+                    <div className="user-opts">
+                        <Button type="gohst" >编辑个人资料</Button>
+                    </div>
+
                 </div>
             </div>
         )
