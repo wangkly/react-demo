dbf9f0cddbae272b93335b7319e40cfca0d036fd
wangkly <wangkly123@gmail.com>
2019-05-04 12:28:13 +0800

    文章样式调整

--- src/home-page/components/news.js
+++ src/home-page/components/news.js
@@ -1,6 +1,9 @@
 import React,{Component} from 'react';
 import {Card,Row,Col} from 'antd';
 const { Meta } = Card;
+import Article from './article';
+
+
 import {withRouter} from "react-router-dom";
 
  class News extends Component{
@@ -11,20 +14,22 @@ import {withRouter} from "react-router-dom";
             <div className="bottom-area">
                 <Row >
                 {
-                    news.map((item)=>{
+                    news.map((item,index)=>{
                         return(
-                            <Col span={6} key={item._id}>
-                            <Card
-                                hoverable
-                                onClick={()=>this.goToArticle(item._id)}
-                                style={{ margin: 10  }}
-                                cover={<img alt="example" src={item.cover || "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"} />}>
-                                <Meta
-                                title={item.title}
-                                // description={item.content}
-                                />
-                            </Card>
-                            </Col>
+                            // <Col span={6} key={item._id}>
+                            // <Card
+                            //     hoverable
+                            //     onClick={()=>this.goToArticle(item._id)}
+                            //     style={{ margin: 10  }}
+                            //     cover={<img alt="example" src={item.cover || "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"} />}>
+                            //     <Meta
+                            //     title={item.title}
+                            //     // description={item.content}
+                            //     />
+                            // </Card>
+                            // </Col>
+
+                            <Article key={index} article={item}/>
                         )
                     })
 
